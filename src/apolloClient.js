1f2804ed778fbfad697f7ff497e15a77bcca5dbb
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-01-28 20:30:52 +0530

    Added the Graphql as already given for the local Server

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   // uri: "http://localhost:4000/graphql",
-  uri: "https://back-end-of-my-user-app.onrender.com/",
+  uri: "https://back-end-of-my-user-app.onrender.com/graphql",
   cache: new InMemoryCache(),
 });
 
