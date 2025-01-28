5a9b655f42efb6addc43fd9c1753ccb06e8a01df
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-01-28 19:03:12 +0530

    Added the live URL of my Backend Server

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  // uri: "http://localhost:4000/graphql",
+  uri: "https://back-end-of-my-user-app.onrender.com/",
   cache: new InMemoryCache(),
 });
 
